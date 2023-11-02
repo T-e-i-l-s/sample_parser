a944ac962f75c8d4bb4a5f95749bda997549ac33
Karim <kfmust@gmail.com>
2023-11-02 13:18:02 +0300

    YAU

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.lazada.com.my/#hp-official-stores'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('span', class_='title')
+
+print(soup)
+print(quotes)
